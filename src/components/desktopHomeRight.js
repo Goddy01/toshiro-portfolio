46dd6303fa3bb891da4e793993d5d166bde09760
Toshihiro <adigungodwin2@gmail.com>
2023-01-06 18:57:53 +0100

    updated the project part of the portfolio

--- src/components/desktopHomeRight.js
+++ src/components/desktopHomeRight.js
@@ -8,6 +8,36 @@ const DesktopHomeRight = () => {
         Projects
       </h2>
       <div className="darken flex flex-col gap-y-8 md:gap-y-3">
+      <div className="flex flex-col bg-box-bg p-10 transition-all duration-300 md:hover:scale-110">
+          <h3 className="text-[1.3rem] font-semibold text-white">
+            FlixGo
+          </h3>
+          <p className="mb-3 mt-1 text-xs font-semibold leading-5 tracking-wider text-light-grey">
+            HTML + CSS + JAVASCRIPT +  + DJANGO + POSTGRESQL + TMDB API
+          </p>
+          <p className="my-3 min-h-[5rem] text-xs font-semibold tracking-wider text-dark-grey md:h-10">
+            This web application utilizes TMDB (The Movie Database) API to retrieve movies and tv shows information. This web application features Watchlist, User Registration and Login, Account Signup Confirmation, User Authentication, Password Reset and more...
+          </p>
+          <div className="flex items-center gap-x-4">
+            <a
+              href="https://flixgo.up.railway.app"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center justify-start gap-x-1 text-xs text-light-grey hover:text-white"
+            >
+              <FaEye /> <span>Live</span>
+            </a>
+            <a
+              href="https://github.com/Goddy01/Movie-App"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-x-1 text-xs text-light-grey hover:text-white"
+            >
+              <FaCodeBranch /> <span>Code</span>
+            </a>
+          </div>
+        </div>
+
         <div className="flex flex-col bg-box-bg p-10 transition-all duration-300 md:hover:scale-110">
           <h3 className="text-[1.3rem] font-semibold text-white">
             Summit E-Shop
@@ -33,36 +63,6 @@ const DesktopHomeRight = () => {
             </a>
             <a
               href="https://github.com/Goddy01/E-commerce"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-x-1 text-xs text-light-grey hover:text-white"
-            >
-              <FaCodeBranch /> <span>Code</span>
-            </a>
-          </div>
-        </div>
-
-        <div className="flex flex-col bg-box-bg p-10 transition-all duration-300 md:hover:scale-110">
-          <h3 className="text-[1.3rem] font-semibold text-white">Bloggie</h3>
-          <p className="mb-3 mt-1 text-xs font-semibold leading-5 tracking-wider text-light-grey">
-            HTML + CSS + JAVASCRIPT + DJANGO + CKEDITOR + SQLITE3 + AWS
-          </p>
-          <p className="my-3 min-h-[5rem] text-xs font-semibold tracking-wider text-dark-grey md:h-10">
-            Bloggie allows users to write and publish blog posts. It features
-            Users Registration and Login, User Authorization and Authentication
-            and more...
-          </p>
-          <div className="flex items-center gap-x-4">
-            <a
-              href="https://bloggie-blogapp.herokuapp.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-x-1 text-xs text-light-grey hover:text-white"
-            >
-              <FaEye /> <span>Live</span>
-            </a>
-            <a
-              href="https://github.com/Goddy01/Blogging-Site-Latest"
               target="_blank"
               rel="noopener noreferrer"
               className="flex items-center gap-x-1 text-xs text-light-grey hover:text-white"
@@ -104,6 +104,37 @@ const DesktopHomeRight = () => {
             </a>
           </div>
         </div>
+
+        <div className="flex flex-col bg-box-bg p-10 transition-all duration-300 md:hover:scale-110">
+          <h3 className="text-[1.3rem] font-semibold text-white">Bloggie</h3>
+          <p className="mb-3 mt-1 text-xs font-semibold leading-5 tracking-wider text-light-grey">
+            HTML + CSS + JAVASCRIPT + DJANGO + CKEDITOR + SQLITE3 + AWS
+          </p>
+          <p className="my-3 min-h-[5rem] text-xs font-semibold tracking-wider text-dark-grey md:h-10">
+            Bloggie allows users to write and publish blog posts. It features
+            Users Registration and Login, User Authorization and Authentication
+            and more...
+          </p>
+          <div className="flex items-center gap-x-4">
+            <a
+              href="https://bloggie-blogapp.herokuapp.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-x-1 text-xs text-light-grey hover:text-white"
+            >
+              <FaEye /> <span>Live</span>
+            </a>
+            <a
+              href="https://github.com/Goddy01/Blogging-Site-Latest"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-x-1 text-xs text-light-grey hover:text-white"
+            >
+              <FaCodeBranch /> <span>Code</span>
+            </a>
+          </div>
+        </div>
+
       </div>
 
       {/* <section>
