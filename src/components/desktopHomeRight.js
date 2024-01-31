2201b33d6ec22bfda13ccdeda3464f5371fa8e07
Goddy01 <adigungodwin2@gmail.com>
2024-01-31 15:31:22 +0100

    updated

--- src/components/desktopHomeRight.js
+++ src/components/desktopHomeRight.js
@@ -9,6 +9,38 @@ const DesktopHomeRight = () => {
       </h2>
       <div className="darken flex flex-col gap-y-8 md:gap-y-3">
         <div className="flex flex-col bg-box-bg p-10 transition-all duration-300 md:hover:scale-110">
+          <h3 className="text-[1.3rem] font-semibold text-white">CrowdSourceIt</h3>
+          <p className="mb-3 mt-1 text-xs font-semibold leading-5 tracking-wider text-light-grey">
+            HTML + CSS + BOOTSTRAP + JAVASCRIPT + jQuery + AJAX +  DJANGO + DJANGO CHANNELS + WEBSOCKET + REDIS + RABBITMQ + CELERY + DOCKER + GIT + POSTGRESQL + PAYSTACK PAYMENT GATEWAY API + GOOGLE OAUTH
+          </p>
+          <p className="my-3 min-h-[5rem] text-xs font-semibold tracking-wider text-dark-grey md:h-16">
+          Revolutionizing crowdsourcing and crowdfunding, this independent web application caters to distinct 
+          user types and introduces features such as a specialized moderator role, robust chat systems, automatic 
+          group chat inclusion, and user-friendly search functionality. Users can share ideas, upvote/downvote 
+          contributions, and mark the most impactful ones. Real-time milestone updates and efficient fund 
+          management enhance the user experience. Technological advancements include Docker for containerization, 
+          Redis for chat system responsiveness, and Celery/RabbitMQ for scalability. The platform is deployed on a serverless infrastructure for optimal performance.
+          </p>
+          <div className="flex items-center gap-x-4">
+            <a
+              href="https://flixgo.up.railway.app"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center justify-start gap-x-1 text-xs text-light-grey hover:text-white"
+            >
+              <FaEye /> <span>Live</span>
+            </a>
+            <a
+              href="https://github.com/Goddy01/Movie-App"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-x-1 text-xs text-light-grey hover:text-white"
+            >
+              <FaCodeBranch /> <span>Code</span>
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-col bg-box-bg p-10 transition-all duration-300 md:hover:scale-110">
           <h3 className="text-[1.3rem] font-semibold text-white">FlixGo</h3>
           <p className="mb-3 mt-1 text-xs font-semibold leading-5 tracking-wider text-light-grey">
             HTML + CSS + JAVASCRIPT + DJANGO + POSTGRESQL + TMDB API + AWS S3 +
