c0d9e7277472197b2ffffc1f4a69c4804a7cf157
Godwin Adigun <adigungodwin2@gmail.com>
2024-02-27 23:38:01 +0100

    Update desktopIntro.js

--- src/components/desktopIntro.js
+++ src/components/desktopIntro.js
@@ -36,19 +36,6 @@ const DesktopIntro = ({ active }) => {
           for{" "}
           <span className="animate-flashTwo text-white opacity-90">learning</span> and a fervent commitment to delivering{" "}
           <span className="animate-flash text-white opacity-90">excellenct, efficient, secure, and scalable web applications</span>. 
-          //   In pursuit of an{" "}
-          // <span className="animate-flashTwo text-white opacity-90">entry-level position</span>, I bring a{" "}
-          // <span className="animate-flash text-white opacity-90">unique blend</span> of{" "}
-          // <span className="animate-flashTwo text-white opacity-90">passion</span> and expertise to a{" "}
-          // <span className="animate-flash text-white opacity-90">challenging and innovative environment</span>. My journey involves shaping{" "}
-          // <span className="animate-flashTwo text-white opacity-90">efficient, secure, and scalable web applications</span>, with a specialized focus on crafting{" "}
-          // <span className="animate-flash text-white opacity-90">robust RESTful APIs</span>. With a{" "}
-          // <span className="animate-flashTwo text-white opacity-90">knack for seamlessly integrating</span>{" "}
-          // <span className="animate-flash text-white opacity-90">third-party APIs</span> and a penchant for{" "}
-          // <span className="animate-flashTwo text-white opacity-90">designing sleek database architectures</span>, I consistently uphold{" "}
-          // <span className="animate-flash text-white opacity-90">industry best practices</span>. Elevate your expectations—I am not just a{" "}
-          // <span className="animate-flashTwo text-white opacity-90">developer</span>; I am a{" "}
-          // <span className="animate-flash text-white opacity-90">catalyst for transformative digital solutions</span>.
         </p>
       </div>
 
