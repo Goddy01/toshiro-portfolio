51ce1cc9ddf6659353f94d5d8ace96ce63802873
Goddy01 <adigungodwin2@gmail.com>
2024-09-23 01:44:08 +0100

    added new card for refnet project

--- src/components/desktopHomeRight.js
+++ src/components/desktopHomeRight.js
@@ -9,6 +9,34 @@ const DesktopHomeRight = () => {
       </h2>
       <div className="darken flex flex-col gap-y-8 md:gap-y-3">
         <div className="flex flex-col bg-box-bg p-10 transition-all duration-300 md:hover:scale-110">
+          <h3 className="text-[1.3rem] font-semibold text-white">Refnet</h3>
+          <p className="mb-3 mt-1 text-xs font-semibold leading-5 tracking-wider text-light-grey">
+            REACT +  DJANGO + DJANGO CHANNELS + WEBSOCKET + REDIS + RABBITMQ + CELERY + GIT + POSTGRESQL + OAUTH + REST API
+          </p>
+          <p className="my-3 min-h-[5rem] text-xs font-semibold tracking-wider text-dark-grey md:h-16">
+          Developed and launched an innovative job referral platform designed to bridge the gap between professionals and job seekers, significantly enhancing employment opportunities in the process
+          </p>
+          <div className="flex items-center gap-x-4 my-8">
+            <a
+              
+              href="https://refnet.onrender.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center justify-start gap-x-1 text-xs text-light-grey hover:text-white"
+            >
+              <FaEye /> <span> </span>
+            </a>
+            {/* <a
+              href=""
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-x-1 text-xs text-light-grey hover:text-white"
+            >
+              <FaCodeBranch /> <span>Code</span>
+            </a> */}
+          </div>
+        </div>
+        <div className="flex flex-col bg-box-bg p-10 transition-all duration-300 md:hover:scale-110">
           <h3 className="text-[1.3rem] font-semibold text-white">CrowdSourceIt</h3>
           <p className="mb-3 mt-1 text-xs font-semibold leading-5 tracking-wider text-light-grey">
             HTML + CSS + BOOTSTRAP + JAVASCRIPT + jQuery + AJAX +  DJANGO + DJANGO CHANNELS + WEBSOCKET + REDIS + RABBITMQ + CELERY + DOCKER + GIT + POSTGRESQL + PAYSTACK PAYMENT GATEWAY API + GOOGLE OAUTH
