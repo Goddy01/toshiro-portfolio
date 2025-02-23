615343bfcfa2a12351d0b922e735e10566a57519
Goddy01 <adigungodwin2@gmail.com>
2025-02-23 18:27:19 +0100

    added boxbox proj

--- src/components/desktopHomeRight.js
+++ src/components/desktopHomeRight.js
@@ -9,6 +9,35 @@ const DesktopHomeRight = () => {
       </h2>
       <div className="darken flex flex-col gap-y-8 md:gap-y-3">
         <div className="flex flex-col bg-box-bg p-10 transition-all duration-300 md:hover:scale-110">
+          <h3 className="text-[1.3rem] font-semibold text-white">Membership-Based Token Vesting</h3>
+          <p className="mb-3 mt-1 text-xs font-semibold leading-5 tracking-wider text-light-grey">
+            RUST + ANCHOR + NODEJS + TYPESCRIPT + REACT + SOLANA + SPL TOKEN + METAPLEX 
+          </p>
+          <p className="my-3 min-h-[5rem] text-xs font-semibold tracking-wider text-dark-grey md:h-16">
+          Designed and implemented a Rust-based Solana smart contract and a website for the F1 Meme (BOXBOX)
+          SPL token, enabling secure token locking and controlled distribution for Formula 1 fan
+          memberships.
+          </p>
+          <div className="flex items-center gap-x-4 my-8">
+            <a
+              href="https://www.f1meme.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-x-1 text-xs text-light-grey hover:text-white"
+            >
+              <FaEye /> <span>Website</span>
+            </a>
+            <a
+              href="https://boxbox-token-vesting.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-x-1 text-xs text-light-grey hover:text-white"
+            >
+              <FaEye /> <span>Token Vesting Website</span>
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-col bg-box-bg p-10 transition-all duration-300 md:hover:scale-110">
           <h3 className="text-[1.3rem] font-semibold text-white">Swap Program</h3>
           <p className="mb-3 mt-1 text-xs font-semibold leading-5 tracking-wider text-light-grey">
             RUST + ANCHOR + TYPESCRIPT + SOLANA
