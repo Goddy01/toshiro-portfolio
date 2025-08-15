4f5afe9570bd455b737ddef21d35783009b7624d
Goddy01 <adigungodwin2@gmail.com>
2025-08-15 05:03:25 +0100

    updated

--- src/components/desktopHomeRight.js
+++ src/components/desktopHomeRight.js
@@ -174,7 +174,7 @@ const DesktopHomeRight = () => {
           </div>
         </div>
         <div className="flex flex-col bg-box-bg p-10 transition-all duration-300 md:hover:scale-110">
-          <h3 className="text-[1.3rem] font-semibold text-white">NFT Collection:</h3>
+          <h3 className="text-[1.3rem] font-semibold text-white">NFT Collection</h3>
           <p className="mb-3 mt-1 text-xs font-semibold leading-5 tracking-wider text-light-grey">
           TYPESCRIPT + SOLANA + METAPLEX + IPFS FILE MANAGEMENT SYSTEM
           </p>
